f6f0e409f9a98ee84f39e427a2c1100b37a4e033
0xFireball <0xFireball@outlook.com>
2017-04-10 12:01:49 -0700

    add devtests projec

--- src/server/Speercs.Server.Tests/Mapgen/MapgenTests.cs
+++ src/server/Speercs.Server.Tests/Mapgen/MapgenTests.cs
@@ -1,13 +1,22 @@
 using System;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Speercs.Server.Tests
 {
     public class MapgenTests
     {
+        private readonly ITestOutputHelper output;
+
+        public MapgenTests(ITestOutputHelper outputHelper)
+        {
+            output = outputHelper;
+        }
+        
         [Fact]
         public void TrueIsTrue()
         {
+            output.WriteLine("true is always true lol");
             Assert.True(true);
         }
     }
