ad5dc18a71e82528ff5349f275fcc9ba198d4127
0xFireball <0xFireball@outlook.com>
2017-04-10 15:42:42 -0700

    implement user registration

---
+++ src/server/Speercs.Server/Services/Auth/AuthCryptoHelper.cs
@@ -0,0 +1,45 @@
+using System.Security.Cryptography;
+using System.Text;
+using Speercs.Server.Services.Auth;
+
+namespace PenguinUpload.Services.Authentication
+{
+    public class AuthCryptoHelper
+    {
+        public PasswordCryptoConfiguration Configuration { get; }
+
+        public AuthCryptoHelper(PasswordCryptoConfiguration conf)
+        {
+            Configuration = conf;
+        }
+
+        public byte[] GenerateSalt()
+        {
+            var len = Configuration.SaltLength;
+            var bytes = new byte[len];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(bytes);
+            }
+            return bytes;
+        }
+
+        private byte[] CalculatePasswordHash(byte[] password, byte[] salt)
+        {
+            var iter = Configuration.Iterations;
+            var len = Configuration.Length;
+            using (var deriveBytes = new Rfc2898DeriveBytes(password, salt, iter))
+            {
+                return deriveBytes.GetBytes(len);
+            }
+        }
+
+        public byte[] CalculateUserPasswordHash(string password, byte[] salt)
+        {
+            var passwordBytes = Encoding.UTF8.GetBytes(password);
+            return CalculatePasswordHash(passwordBytes, salt);
+        }
+
+        public const int DefaultApiKeyLength = 42;
+    }
+}
--- src/server/Speercs.Server/Services/Auth/UserManagerService.cs
+++ src/server/Speercs.Server/Services/Auth/UserManagerService.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Security;
 using System.Threading.Tasks;
 using LiteDB;
+using PenguinUpload.Services.Authentication;
 using Speercs.Server.Configuration;
+using Speercs.Server.Models.Requests;
 using Speercs.Server.Models.User;
+using Speercs.Server.Utilities;
 
 namespace  Speercs.Server.Services.Auth
 {
@@ -20,9 +24,39 @@ namespace  Speercs.Server.Services.Auth
             return Task.FromResult(userCollection.FindOne(x => x.Username == username));
         }
 
-        public Task<RegisteredUser> RegisterUserAsync(UserRegistrationRequest regRequest)
+        public async Task<RegisteredUser> RegisterUserAsync(UserRegistrationRequest regRequest)
         {
-            
+            if (await FindUserByUsernameAsync(regRequest.Username) != null) throw new SecurityException("A user with the same username already exists!");
+            return await Task.Run(() => 
+            {
+                // Calculate cryptographic info
+                var cryptoConf = PasswordCryptoConfiguration.CreateDefault();
+                var cryptoHelper = new AuthCryptoHelper(cryptoConf);
+                var pwSalt = cryptoHelper.GenerateSalt();
+                var encryptedPassword =
+                    cryptoHelper.CalculateUserPasswordHash(regRequest.Password, pwSalt);
+                // Create user
+                var userRecord = new RegisteredUser
+                {
+                    Identifier = Guid.NewGuid().ToString(),
+                    Username = regRequest.Username,
+                    ApiKey = StringUtils.SecureRandomString(AuthCryptoHelper.DefaultApiKeyLength),
+                    Crypto = new ItemCrypto
+                    {
+                        Salt = pwSalt,
+                        Conf = cryptoConf,
+                        Key = encryptedPassword
+                    },
+                };
+                // Add the user to the database
+                userCollection.Insert(userRecord);
+
+                // Index database
+                userCollection.EnsureIndex(x => x.Identifier);
+                userCollection.EnsureIndex(x => x.ApiKey);
+                userCollection.EnsureIndex(x => x.Username);
+                return userRecord;
+            });
         }
     }
 }
