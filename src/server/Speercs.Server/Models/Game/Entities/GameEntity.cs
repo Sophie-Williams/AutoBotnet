f87a975ffcc925655a15a285376bad6a1d20186e
0xFireball <0xFireball@outlook.com>
2017-04-11 11:45:48 -0700

    add game entities

--- src/server/Speercs.Server/Models/Game/Entities/GameEntity.cs
+++ src/server/Speercs.Server/Models/Game/Entities/GameEntity.cs
@@ -1,8 +1,14 @@
 
+using Speercs.Server.Models.Math;
+
 namespace Speercs.Server.Models.Game.Entities
 {
     public abstract class GameEntity
     {
-        
+        public string MapIdentifier { get; set; }
+
+        public Point Location { get; set; }
+
+        public Point Size { get; set; }
     }
 }
