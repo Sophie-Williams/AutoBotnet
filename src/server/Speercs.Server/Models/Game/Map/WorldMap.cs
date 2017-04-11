d4b4f2969d892fec6caa39df6d3bc6fd3e3df5b1
0xFireball <0xFireball@outlook.com>
2017-04-11 12:10:56 -0700

    add worldmap data

---
+++ src/server/Speercs.Server/Models/Game/Map/WorldMap.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace Speercs.Server.Models.Game.Map
+{
+    public class WorldMap
+    {
+        public Dictionary<string, Room> RoomDict { get; set; }
+
+        public void Initialize()
+        {
+            RoomDict = new Dictionary<string, Room>();
+        }
+
+        public Room this[int x, int y] => RoomDict[$"{x}:{y}"];
+    }
+}
