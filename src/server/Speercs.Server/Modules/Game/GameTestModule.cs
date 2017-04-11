305411876cbe6596dbfc83dc5a14f39a91ad7e58
0xFireball <0xFireball@outlook.com>
2017-04-11 15:34:59 -0700

    refactor authenticated api modules

--- src/server/Speercs.Server/Modules/Game/GameTestModule.cs
+++ src/server/Speercs.Server/Modules/Game/GameTestModule.cs
@@ -4,24 +4,12 @@ using Speercs.Server.Services.Auth;
 
 namespace Speercs.Server.Modules.Game
 {
-    public class GameTestModule : SBaseModule
+    public class GameTestModule : UserApiModule
     {
-        private UserManagerService userManager;
 
-        public ISContext ServerContext { get; private set; }
-
-        public GameTestModule(ISContext serverContext) : base("/game/test")
+        public GameTestModule(ISContext serverContext) : base("/game/test", serverContext)
         {
-            ServerContext = serverContext;
-
-            this.RequiresAuthentication();
-
-            Before += ctx =>
-            {
-                userManager = new UserManagerService(ServerContext);
-                return null;
-            };
-            Get("/", async _ => (await userManager.FindUserByIdentifierAsync(Context.CurrentUser.Identity.Name)).Username);
+            Get("/", async _ => (await UserManager.FindUserByIdentifierAsync(Context.CurrentUser.Identity.Name)).Username);
         }
     }
 }
