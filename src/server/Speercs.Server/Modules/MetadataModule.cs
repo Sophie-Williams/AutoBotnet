890c2afbf856bdf637934ed3651e25bf0f95bcc6
Joona <joonatoona@digitalfishfun.com>
2017-04-12 10:05:20 -0700

    Stuff and things.

--- src/server/Speercs.Server/Modules/MetadataModule.cs
+++ src/server/Speercs.Server/Modules/MetadataModule.cs
@@ -5,6 +5,7 @@ namespace Speercs.Server.Modules
         public MetadataModule() : base("/meta")
         {
             Get("/", _ => "Speercs Server Pre-Alpha");
+            Get("/version", _ => string.Format("Speercs Server v{0} running on .NET core v{1}","1.0.0","1.0.1")); // TODO: Get version numbers automagically
         }
     }
 }
