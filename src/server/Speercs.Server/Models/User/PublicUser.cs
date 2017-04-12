a76c5694d0ecbab99bdf58fe20f742838a8f25d3
0xFireball <0xFireball@outlook.com>
2017-04-12 14:17:59 -0700

    allow fetching public profile

---
+++ src/server/Speercs.Server/Models/User/PublicUser.cs
@@ -0,0 +1,18 @@
+using Newtonsoft.Json;
+
+namespace Speercs.Server.Models.User
+{
+    /// <summary>
+    /// Public user data model
+    /// </summary>
+    public class PublicUser : DatabaseObject
+    {
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        public PublicUser(RegisteredUser user)
+        {
+            Username = user.Username;
+        }
+    }
+}
--- src/server/Speercs.Server/Modules/Game/UserMetadataModule.cs
+++ src/server/Speercs.Server/Modules/Game/UserMetadataModule.cs
@@ -1,4 +1,7 @@
+using Nancy;
 using Speercs.Server.Configuration;
+using Speercs.Server.Models.User;
+using Speercs.Server.Utilities;
 
 namespace Speercs.Server.Modules.Game
 {
@@ -7,6 +10,13 @@ namespace Speercs.Server.Modules.Game
         public UserMetadataModule(ISContext serverContext) : base("/game/umeta", serverContext)
         {
             Get("/me", async _ => (await UserManager.FindUserByIdentifierAsync(Context.CurrentUser.Identity.Name)).Username);
+            Get("/user/{id}", async args => 
+            {
+                var user = await UserManager.FindUserByIdentifierAsync(((string)args.id));
+                if (user == null) return HttpStatusCode.NotFound;
+                var publicProfile = new PublicUser(user);
+                return Response.AsJsonNet(publicProfile);
+            });
         }
     }
 }
