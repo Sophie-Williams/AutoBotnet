865e70c9e258225fad6f005d8a0598baba8807e3
Joona <joonatoona@digitalfishfun.com>
2017-04-12 15:30:18 -0700

    Wot?!?

--- src/server/Speercs.Server/Modules/Game/MapAccessModule.cs
+++ src/server/Speercs.Server/Modules/Game/MapAccessModule.cs
@@ -15,21 +15,23 @@ namespace Speercs.Server.Modules.Game
             Get("/", async _ => (await UserManager.FindUserByIdentifierAsync(Context.CurrentUser.Identity.Name)).Username);
             Get("/room", _ =>
             {
-                if (!int.TryParse(Request.Query['x'], out int x)) return HttpStatusCode.BadRequest;
-                if (!int.TryParse(Request.Query['y'], out int y)) return HttpStatusCode.BadRequest;
+                int x;
+                int y;
+                if (!int.TryParse(Request.Query['x'], out x)) return HttpStatusCode.BadRequest;
+                if (!int.TryParse(Request.Query['y'], out y)) return HttpStatusCode.BadRequest;
                 var room = ServerContext.AppState.WorldMap[x, y];
                 if (room == null) return HttpStatusCode.NotFound;
                 return Response.AsJsonNet(room);
             });
-            // Post("/genroom", _ =>
-            // {
-            //     MapGenerator mapGen = new MapGenerator();
-            //     var req = this.Bind<RoomGenerationRequest>();
-            //     if (ServerContext.AppState.WorldMap.RoomDict[$"{req.x}:{req.y}"] != null) return HttpStatusCode.BadRequest;
-            //     Room newRoom = mapGen.GenerateRoom(req.density);
-            //     ServerContext.AppState.WorldMap.RoomDict.Add($"{req.x}:{req.y}", newRoom);
-            //     return Response.AsJson(newRoom);
-            // });
+            Post("/genroom", _ =>
+            {
+                MapGenerator mapGen = new MapGenerator();
+                var req = this.Bind<RoomGenerationRequest>();
+                if (ServerContext.AppState.WorldMap[req.X, req.Y] != null) return HttpStatusCode.BadRequest;
+                Room newRoom = mapGen.GenerateRoom(req.Density);
+                ServerContext.AppState.WorldMap.RoomDict.Add($"{req.X}:{req.Y}", newRoom);
+                return Response.AsJson(newRoom);
+            });
         }
     }
 }
