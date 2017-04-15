36cf5fb702453fed78f9fc2cbc9c82c5a0cd6e96
Joona <joonatoona@digitalfishfun.com>
2017-04-15 13:48:43 -0700

    Base client class :D

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -1,18 +1,29 @@
 class SpeercsApi {
   constructor(endpoint, apiKey) {
     this.endpoint = endpoint;
-    this.serverInfo = { 
-      name: "Loading...", motd: "Loading..." 
+    this.username = null;
+    this.apiKeyValid = false;
+    this.serverInfo = {
+      name: "Loading...", motd: "Loading..."
     };
     this.apiKey = apiKey;
     this.axios = axios.create({
-      baseURL: this.endpoint,
+      baseURL: this.endpoint + "/a/",
       params: {
         apikey: this.apiKey
       },
       responseType: 'json'
+    });
+  }
+  updateInfo() {
+    axios.get(this.endpoint + "/meta").then((res) => {
+      this.serverInfo = res.data;
+    }).catch((err) => {
+      throw err;
+    });
+    this.axios.get("/game/umeta/me", { responseType: 'text' }).then((res) => {
+      if (res.status != 200) return apiKeyValid = false;
+      this.username = res.data.username;
     })
-
-    
   }
 }
