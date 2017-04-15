403829a0c61b42659fb39011fb279da6f1e0c6f6
0xFireball <0xFireball@outlook.com>
2017-04-14 19:08:14 -0700

    fix some problems with api changes and missing usings

--- src/server/Speercs.Server/Modules/Admin/AdminMapAccessModule.cs
+++ src/server/Speercs.Server/Modules/Admin/AdminMapAccessModule.cs
@@ -1,6 +1,8 @@
 using Nancy;
+using Nancy.ModelBinding;
 using Speercs.Server.Configuration;
 using Speercs.Server.Game.MapGen;
+using Speercs.Server.Models.Requests;
 
 namespace Speercs.Server.Modules.Admin
 {
@@ -13,7 +15,7 @@ namespace Speercs.Server.Modules.Admin
                 MapGenerator mapGen = new MapGenerator(ServerContext);
                 var req = this.Bind<RoomGenerationRequest>();
                 if (ServerContext.AppState.WorldMap[req.X, req.Y] != null) return HttpStatusCode.BadRequest;
-                var newRoom = mapGen.GenerateRoom(req.Density);
+                var newRoom = mapGen.GenerateRoom(req.X, req.Y, req.Density);
                 ServerContext.AppState.WorldMap[req.X, req.Y] = newRoom;
                 return Response.AsJson(newRoom);
             });
