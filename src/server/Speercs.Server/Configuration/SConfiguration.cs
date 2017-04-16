d9f7474cd15963abb3871814668e31a81d3ffc98
0xFireball <0xFireball@outlook.com>
2017-04-15 22:03:14 -0700

    Revert "remove unused GameName"
    This reverts commit f8c6e281260e86c0c6d287185f6eeea2ffec19cf.

--- src/server/Speercs.Server/Configuration/SConfiguration.cs
+++ src/server/Speercs.Server/Configuration/SConfiguration.cs
@@ -16,6 +16,9 @@ namespace Speercs.Server.Configuration
         [JsonIgnore]
         public string BaseDirectory { get; set; }
 
+        [JsonIgnore]
+        public string GameName { get; } = "Speercs";
+
         [JsonProperty("globalName")]
         public string GlobalName { get; set; } = "Speercs";
 
