259db4d7e7c5dfb9d45909d10e8191dc2021843c
0xFireball <0xFireball@outlook.com>
2017-04-16 23:08:38 -0700

    add auth dependent toolbar links

--- src/server/Speercs.Server/ClientApp/src/App.vue
+++ src/server/Speercs.Server/ClientApp/src/App.vue
@@ -8,30 +8,17 @@
       <v-sidebar v-model="sidebar_v" height="auto">
         <v-list dense>
           <template v-for="(item, i) in sidebar">
-            <v-list-group v-if="item.items">
-              <v-list-item slot="item">
-                <v-list-tile ripple>
-                  <v-list-tile-title v-text="item.title" />
-                  <v-list-tile-action>
-                    <v-icon>keyboard_arrow_down</v-icon>
-                  </v-list-tile-action>
-                </v-list-tile>
-              </v-list-item>
-              <v-list-item v-for="(subItem,i) in item.items" :key="i">
-                <v-list-tile ripple>
-                  <v-list-tile-title v-text="subItem.title" />    
-                </v-list-tile>
-              </v-list-item>
-            </v-list-group>
-            <v-subheader v-else-if="item.header" v-text="item.header" />
+            <v-subheader v-if="item.header" v-text="item.header" />
             <v-divider v-else-if="item.divider" light />
             <v-list-item v-else>
-              <v-list-tile ripple>
-                <v-list-tile-avatar>
-                  <v-icon>{{ item.avatar }}</v-icon>
-                </v-list-tile-avatar>
-                <v-list-tile-title v-text="item.title" />    
-              </v-list-tile>
+              <template v-if="!item.authRequired || (item.authRequired && loggedIn) || (item.unauthRequired && !loggedIn)">
+                <v-list-tile ripple>
+                  <v-list-tile-avatar>
+                    <v-icon>{{ item.avatar }}</v-icon>
+                  </v-list-tile-avatar>
+                  <v-list-tile-title v-text="item.title" />    
+                </v-list-tile>
+              </template>
             </v-list-item>
           </template>
         </v-list>
@@ -62,8 +49,21 @@
           //     { title: 'Child' }
           //   ]
           // },
-          { title: 'Dashboard', avatar: 'home', authRequired: true },
-          { title: 'Account', avatar: 'person', authRequired: true },
+          { 
+            title: 'Dashboard',
+            avatar: 'home',
+            authRequired: true
+          },
+          {
+            title: 'Account',
+            avatar: 'person',
+            authRequired: true
+          },
+          {
+            title: 'Login',
+            avatar: 'person',
+            unauthRequired: true
+          },
           { divider: true },
           { header: 'Support' },
           { title: 'Report Bugs', avatar: 'error' }
