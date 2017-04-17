cd8c93dde1d0bea501255ffbf2d4873fc76ec822
0xFireball <0xFireball@outlook.com>
2017-04-16 18:21:56 -0700

    output to ../dist

--- src/server/Speercs.Server/ClientApp/webpack.config.js
+++ src/server/Speercs.Server/ClientApp/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     main: './src/main.js'
   },
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, '../dist'),
     publicPath: '/dist/',
     filename: 'build.js'
   },
