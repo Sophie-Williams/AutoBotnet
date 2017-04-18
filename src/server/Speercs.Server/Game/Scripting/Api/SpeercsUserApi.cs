3537efff5b96b96ddb461f9ee971b19544c180f2
Joona <joonatoona@digitalfishfun.com>
2017-04-18 14:29:29 -0700

    Am I doing this right?

--- src/server/Speercs.Server/Game/Scripting/Api/SpeercsUserApi.cs
+++ src/server/Speercs.Server/Game/Scripting/Api/SpeercsUserApi.cs
@@ -1,11 +1,27 @@
+using System.Collections.Generic;
+using Speercs.Server.Configuration;
+using Speercs.Server.Models.Game.Entities;
+
 namespace Speercs.Server.Game.Scripting.Api
 {
     public class SpeercsUserApi
     {
         public string UserId { get; }
+        public string RoomLocation { get; set; }
+        private ISContext ServerContext { get; set; }
 
-        public SpeercsUserApi(string userId) {
+        public SpeercsUserApi(string userId, ISContext serverContext)
+        {
             UserId = userId;
+            ServerContext = serverContext;
+        }
+
+        public List<GameEntity> Entities
+        {
+            get
+            {
+                return ServerContext.AppState.Entities.GetAllByUser(ServerContext.AppState.PlayerData[UserId]);
+            }
         }
     }
 }
