c974f0a515a57553ef36dad3040b9600d6a0075a
0xFireball <0xFireball@outlook.com>
2017-05-31 17:08:58 -0700

    add gamecode vuex actions

--- src/server/Speercs.Server/ClientApp/src/store/gamecode.js
+++ src/server/Speercs.Server/ClientApp/src/store/gamecode.js
@@ -2,10 +2,14 @@ const state = {
 }
 
 const actions = {
-  deploy_code ({commit, state}, args) {
-    return new Promise((resolve, reject) => {
-      
-    })
+  deploy_user_code ({commit, state}, args) {
+    return args.api.deployUserCode(args.source)
+  },
+  get_user_code ({commit, state}, args) {
+    return args.api.getUserCode()
+  },
+  reload_user_code ({commit, state}, args) {
+    return args.api.reloadUserCode()
   }
 }
 
