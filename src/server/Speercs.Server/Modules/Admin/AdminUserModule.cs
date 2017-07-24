0452c2d06d0cf01b1ca5c9d5b1fcba55c71e8ca2
Joona <joonatoona@digitalfishfun.com>
2017-07-23 18:31:26 -0700

    start on admin user api

---
+++ src/server/Speercs.Server/Modules/Admin/AdminUserModule.cs
@@ -0,0 +1,24 @@
+using Nancy;
+using Nancy.ModelBinding;
+using Speercs.Server.Configuration;
+using Speercs.Server.Models.Requests;
+using Speercs.Server.Services.Auth;
+using Speercs.Server.Utilities;
+using Speercs.Server.Models.User;
+
+namespace Speercs.Server.Modules.Admin
+{
+    public class AdminUserModule : AdminApiModule
+    {
+        public AdminUserModule(ISContext serverContext) : base("/user", serverContext)
+        {
+            Get("/{id}", async args => {
+                var userManager = new UserManagerService(ServerContext);
+                var user = await userManager.FindUserByIdentifierAsync(args.id);
+                if (user == null) return HttpStatusCode.NotFound;
+                return Response.AsJsonNet((RegisteredUser)user);
+            });
+        }
+    }
+}
+
