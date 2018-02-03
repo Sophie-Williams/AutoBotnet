ab3d1b278e83aac70cd96e2e78b95d9dc685fcc4
0xFireball <0xFireball@outlook.com>
2018-02-02 22:48:49 -0800

    remove old scriptapi backends and use scriptmodules

---
+++ src/server/Speercs.Server/Game/Scripting/Api/ScriptingApiModule.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using IridiumJS;
+using IridiumJS.Native;
+using IridiumJS.Native.Object;
+using IridiumJS.Runtime.Descriptors;
+using IridiumJS.Runtime.Interop;
+using Speercs.Server.Configuration;
+
+namespace Speercs.Server.Game.Scripting.Api {
+    public class ScriptingApiModule : ObjectInstance {
+        protected readonly ISContext _context;
+        protected readonly string _userId;
+        protected List<string> myEntities;
+
+        public ScriptingApiModule(JSEngine engine, ISContext context, string userId) : base(engine) {
+            _context = context;
+            _userId = userId;
+            myEntities = context.AppState.PlayerData[userId].OwnedEntities;
+            SetDefaultToString();
+        }
+
+        protected PropertyDescriptor MakeFunctionProperty(JSEngine engine, Delegate func) {
+            return new PropertyDescriptor(new DelegateWrapper(engine, func), false, false, false);
+        }
+
+        protected void DefineFunction<T>(string name, Func<T> func) {
+            FastSetProperty(name, MakeFunctionProperty(Engine, func));
+        }
+
+        protected void DefineFunction(string name, Delegate func) {
+            FastSetProperty(name, MakeFunctionProperty(Engine, func));
+        }
+
+        protected void AddGlobal(string name, object value) {
+            Engine.Global.FastAddProperty(name, JsValue.FromObject(Engine, value), false, true, false);
+        }
+
+        private void SetDefaultToString() {
+            DefineFunction("toString", () => $"[object {Class}]");
+        }
+    }
+}
--- src/server/Speercs.Server/Game/Scripting/ScriptExecutor.cs
+++ src/server/Speercs.Server/Game/Scripting/ScriptExecutor.cs
@@ -11,30 +11,7 @@ namespace Speercs.Server.Game.Scripting {
             UserIdentifier = userIdentifier;
         }
 
-        public BotApi GetBotObject(Bot bot) {
-            return botObjects.GetOrAdd(bot.ID, key => {
-                return new BotApi(this, bot);
-            });
-        }
-
-        public bool RemoveBot(Bot bot) {
-            return RemoveBot(bot.ID);
-        }
-
-        public bool RemoveBot(string botId) {
-            return botObjects.TryRemove(botId, out var removed);
-        }
-
-        public RoomApi GetRoomObject(int roomX, int roomY) {
-            return roomObjects.GetOrAdd($"{roomX}:{roomY}", key => {
-                return new RoomApi(this, ServerContext.AppState.WorldMap[roomX, roomY]);
-            });
-        }
-
         public JSEngine Engine { get; }
         public string UserIdentifier { get; }
-
-        private ConcurrentDictionary<string, BotApi> botObjects = new ConcurrentDictionary<string, BotApi>();
-        private ConcurrentDictionary<string, RoomApi> roomObjects = new ConcurrentDictionary<string, RoomApi>();
     }
 }
