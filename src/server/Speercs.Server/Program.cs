63a47af0c27c1466b72efa380bef340f41c2dceb
0xFireball <0xFireball@outlook.com>
2018-02-05 12:06:49 -0800

    add commandline configuration for aspnetcore

--- src/server/Speercs.Server/Program.cs
+++ src/server/Speercs.Server/Program.cs
@@ -1,10 +1,20 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace Speercs.Server {
     public class Program {
         public static void Main(string[] args) {
+            // boot ASP.NET Core app
+
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "hosting.json"), true)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();
+
             var host = new WebHostBuilder()
+                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
