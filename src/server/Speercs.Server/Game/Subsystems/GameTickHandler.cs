67f735452c97c95c618fcba7073759b018c88237
0xFireball <0xFireball@outlook.com>
2018-02-09 22:42:21 -0800

    abstract program execution

--- src/server/Speercs.Server/Game/Subsystems/GameTickHandler.cs
+++ src/server/Speercs.Server/Game/Subsystems/GameTickHandler.cs
@@ -1,57 +1,15 @@
-using System;
-using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
-using IridiumJS.Native;
 using Speercs.Server.Configuration;
 using Speercs.Server.Game.Scripting;
-using Speercs.Server.Services.Application;
-using Speercs.Server.Services.Game;
 
-namespace Speercs.Server.Game.Subsystems {
+namespace Speercs.Server.Game.Subsystems
+{
     public class GameTickHandler : DependencyObject {
         public GameTickHandler(ISContext context) : base(context) { }
 
         public async Task onTickAsync() {
-            try {
-                serverContext.appState.tickCount++;
-                var executors =
-                    serverContext.appState.userMetrics.Select(x => serverContext.executors.retrieveExecutor(x.Key))
-                        .OrderBy(a => Guid.NewGuid()).ToList();
-                foreach (var executor in executors) {
-                    if (executor == null) continue;
-                    await Task.Run(() => {
-                        var completionWait = new ManualResetEventSlim();
-                        var executionHost = new ScriptExecutionHost(executor.engine, executor.userIdentifier, completionWait);
-                        var executionThread = new Thread(executionHost.execute);
-                        executionThread.Start();
-                        completionWait.Wait();
-                        switch (executionHost.exception) {
-                            case null:
-                            serverContext.log.writeLine(
-                                $"Player {executor.userIdentifier} program executed successfully with result {executionHost.result}",
-                                SpeercsLogger.LogLevel.Trace);
-                                break;
-                            case TimeoutException ex: {
-                                throw new TimeoutException($"Player {executor.userIdentifier} code took too long", ex);
-                            }
-                            case OutOfMemoryException ex: {
-                                throw new CodeExecutionException($"Player {executor.userIdentifier} program killed for exceeding memory limit", ex);
-                            }
-                            case Exception ex: {
-                                throw new CodeExecutionException(
-                                    $"Error executing player {executor.userIdentifier} program", ex);
-                            }
-                        }
-                    });
-                }
-
-                // TODO: Tick all entities
-
-                // Game update logic (state: won)
-            } catch (Exception ex) {
-                serverContext.log.writeLine(ex.ToString(), SpeercsLogger.LogLevel.Warning);
-            }
+            var batchExecutor = new BatchProgramExecutor(serverContext);
+            await batchExecutor.executePlayerPrograms();
         }
     }
 }
