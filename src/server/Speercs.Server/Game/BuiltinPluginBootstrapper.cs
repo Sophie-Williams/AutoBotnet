0d6b079a14b7a67465bad216cdf8599d09a26acd
0xFireball <0xFireball@outlook.com>
2018-05-06 15:47:41 -0700

    automagically load built in plugins

--- src/server/Speercs.Server/Game/BuiltinPluginBootstrapper.cs
+++ src/server/Speercs.Server/Game/BuiltinPluginBootstrapper.cs
@@ -1,16 +1,13 @@
-using System.Runtime.Loader;
+using System.Reflection;
 using Speercs.Server.Configuration;
-using Speercs.Server.Game.MapGen.Features;
-using Speercs.Server.Models.Entities;
 
 namespace Speercs.Server.Game {
     public class BuiltinPluginBootstrapper : DependencyObject {
         public BuiltinPluginBootstrapper(ISContext context) : base(context) { }
 
         public void loadAll() {
-            // Register default plugins
-            serverContext.pluginLoader.load<DefaultFeaturesPlugin>();
-            serverContext.pluginLoader.load<DefaultBotTemplatesPlugin>();
+            // Automatically load plugins from this assembly
+            serverContext.pluginLoader.loadFrom(Assembly.GetExecutingAssembly());
 
             // Load plugin assemblies
             foreach (var pluginAssemblyPath in serverContext.configuration.pluginAssemblies) {
