60508a6798d639931f3d3097a42584dd888ce80b
0xFireball <0xFireball@outlook.com>
2018-05-08 09:22:29 -0700

    check entities in map fetch

--- src/server/Speercs.Server/Web/Realtime/Handlers/Game/MapFetchRealtimeHandler.cs
+++ src/server/Speercs.Server/Web/Realtime/Handlers/Game/MapFetchRealtimeHandler.cs
@@ -1,6 +1,8 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using Speercs.Server.Configuration;
+using Speercs.Server.Models.Math;
 
 namespace Speercs.Server.Web.Realtime.Handlers {
     public class MapFetchRealtimeHandler : RealtimeHandler {
@@ -10,6 +12,12 @@ namespace Speercs.Server.Web.Realtime.Handlers {
             var dataBundle = (JObject) data;
             var x = dataBundle["x"].ToObject<int>();
             var y = dataBundle["y"].ToObject<int>();
+            // ensure that the user can "see" the room
+            if (!serverContext.appState.entities.getByRoom(new Point(x, y))
+                .Any(nt => nt.teamId == rtContext.userIdentifier)) {
+                    // fail
+                    return Task.FromResult<JToken>(JValue.CreateNull());
+            }
             var room = serverContext.appState.worldMap[x, y];
             if (room == null) return Task.FromResult<JToken>(JValue.CreateNull());
             return Task.FromResult<JToken>(JObject.FromObject(room));
