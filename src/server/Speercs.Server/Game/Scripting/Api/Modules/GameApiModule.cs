0619f21f2829feea8e3c647c63200df078e79a95
0xFireball <0xFireball@outlook.com>
2018-05-07 17:48:40 -0700

    now supports late-delivered messages!

--- src/server/Speercs.Server/Game/Scripting/Api/Modules/GameApiModule.cs
+++ src/server/Speercs.Server/Game/Scripting/Api/Modules/GameApiModule.cs
@@ -1,10 +1,19 @@
-﻿using IridiumJS;
+﻿using System;
+using IridiumJS;
+using Newtonsoft.Json.Linq;
 using Speercs.Server.Configuration;
 
 namespace Speercs.Server.Game.Scripting.Api.Modules {
     public class GameApiModule : ScriptingApiModule {
         public GameApiModule(JSEngine engine, ISContext context, string userId) : base(engine, context, userId) {
+            bool push(object obj) {
+                var pushMessageTask = context.notificationPipeline.pushMessageAsync(JToken.FromObject(obj),
+                        userId, true);
+                return true;
+            }
+
             defineFunction("getUserIdentifier", () => this.userId);
+            defineFunction("push", new Func<object, bool>(push));
         }
     }
 }
