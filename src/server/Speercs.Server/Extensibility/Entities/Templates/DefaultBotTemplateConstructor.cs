79be5658c248d0851376fd40375d2302e6389e2a
0xFireball <0xFireball@outlook.com>
2018-08-18 18:35:14 -0700

    add a ranger bot

---
+++ src/server/Speercs.Server/Extensibility/Entities/Templates/DefaultBotTemplateConstructor.cs
@@ -0,0 +1,19 @@
+﻿using Speercs.Server.Models;
+using Speercs.Server.Models.Entities;
+using Speercs.Server.Models.Entities.Buildings;
+
+namespace Speercs.Server.Extensibility.Entities.Templates {
+    public static class DefaultBotTemplateConstructor {
+        public static Bot construct(FactoryBuilding factory, UserEmpire team, IBotTemplate template) {
+            var bot = new Bot(factory.position, team) {
+                model = template.name,
+                coreCapacity = template.coreCapacity,
+                reactorPower = template.reactorPower,
+                memory = new int[template.memorySize],
+                moveCost = template.movecost
+            };
+            bot.resetHealth(template.health);
+            return bot;
+        }
+    }
+}
---
+++ src/server/Speercs.Server/Extensibility/Entities/Templates/ExplorationBots.cs
@@ -0,0 +1,31 @@
+﻿using Speercs.Server.Models;
+using Speercs.Server.Models.Entities;
+using Speercs.Server.Models.Entities.Buildings;
+
+namespace Speercs.Server.Extensibility.Entities.Templates {
+    public class ScoutBotTemplate : IBotTemplate {
+        public (string, long)[] costs { get; } = {("nrg", 40)};
+        public string name => "scout";
+        public int coreCapacity => 4;
+        public int reactorPower => 20;
+        public int memorySize => 32;
+        public int health => 10;
+        public int movecost => 0;
+
+        public Bot construct(FactoryBuilding factory, UserEmpire team) =>
+            DefaultBotTemplateConstructor.construct(factory, team, this);
+    }
+
+    public class RangerBotTemplate : IBotTemplate {
+        public (string, long)[] costs { get; } = {("nrg", 80)};
+        public string name => "ranger";
+        public int coreCapacity => 6;
+        public int reactorPower => 30;
+        public int memorySize => 32;
+        public int health => 20;
+        public int movecost => 0;
+
+        public Bot construct(FactoryBuilding factory, UserEmpire team) =>
+            DefaultBotTemplateConstructor.construct(factory, team, this);
+    }
+}
